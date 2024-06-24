bccfa4cbb084e43d45bd02355d41f5f2d033aae1
vamsimadhav <vamsimadhav2000@gmail.com>
2024-06-24 10:16:01 +0530

    added a delay for testing

--- deep_link_manager.js
+++ deep_link_manager.js
@@ -2,8 +2,11 @@ import branch from "react-native-branch";
 
 export const sendAppInstanceIdToBranch = async () => {
     //const appInstanceId = (await analytics().getAppInstanceId()) ?? '';
-    branch.setRequestMetadata('$firebase_app_instance_id', 1234);
-    branch.subscribe(() => {
-        console.log("called from inside subscribe")
-    });
+
+    branch.setRequestMetadata('$firebase_app_instance_id', '1234');
+    setTimeout(() => {
+        branch.subscribe(() => {
+            console.log("called from inside subscribe");
+        });
+    }, 5000);
     };
